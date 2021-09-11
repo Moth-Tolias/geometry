aac7644589b06348611749a856ccdffab2cea5d3
Su <moth.tolias@gmail.com>
2021-09-11 01:06:35 +0100

    generic rectangles, finally

--- source/rectangle.d
+++ source/rectangle.d
@@ -1,41 +1,41 @@
 module geometry.rectangle;
 import geometry.vector;
 
-struct Rectangle
+struct Rectangle(T)
 {
-	Vector2!(int) position;
-	Vector2!(int) size;
+	Vector2!T position;
+	Vector2!T size;
 
-	//TODO: d properties aren't  properly supported yet
-	@safe nothrow pure int x() const { return position.x; }
-	@safe nothrow pure void x(in int val) { position.x = val; }
+	invariant(size.x >= 0);
+	invariant(size.y >= 0);
 
-	@safe nothrow pure int y() const { return position.y; }
-	@safe nothrow pure void y(int val) { position.y = val; }
+	@property @safe @nogc nothrow pure T x() const { return position.x; }
+	@property @safe @nogc nothrow void x(in T rhs) { position.x = rhs; }
 
-	@safe nothrow pure int w() const  { return size.x; }
-	@safe nothrow pure void w(int val) { size.x = val; }
+	@property @safe @nogc nothrow pure T y() const { return position.y; }
+	@property @safe @nogc nothrow void y(in T rhs) { position.y = rhs; }
 
-	@safe nothrow pure int h() const { return size.y; }
-	@safe nothrow pure void h(int val) { size.y = val; }
+	@property @safe @nogc nothrow pure T w() const  { return size.x; }
+	@property @safe @nogc nothrow void w(in T rhs) { size.x = rhs; }
+
+	@property @safe @nogc nothrow pure T h() const { return size.y; }
+	@property @safe @nogc nothrow void h(in T rhs) { size.y = rhs; }
 
 	alias width = w;
 	alias height = h;
-
 }
 
-static bool pointInRectangle(in Rectangle r, in Vector2!(int) point) pure @safe nothrow
+static bool pointInRectangle(R : Rectangle!(T), T)(in R r, in Vector2!(T) point) pure @safe @nogc nothrow
 {
-	return ((point.x >= r.x) && point.x <= r.x + r.w) && ((point.y >= r.y) && point.y <= r.y + r.h);
+	return ((point.x >= r.x) && (point.x <= (r.x + r.w))) && ((point.y >= r.y) && (point.y <= (r.y + r.h)));
 }
 
 unittest
 {
-	immutable auto r = Rectangle(Vector2!(int)(0), Vector2!(int)(32));
+	immutable auto r = Rectangle!int(Vector2!(int)(0), Vector2!(int)(32));
 	assert(r.pointInRectangle(Vector2!(int)(16)));
 	assert(r.pointInRectangle(Vector2!(int)(0)));
 	assert(r.pointInRectangle(Vector2!(int)(32)));
 
 	assert(! r.pointInRectangle(Vector2!(int)(-16)));
 }
-
