132a8b8b2bede8cb3fdd7067568a80e6344448a5
Su <moth.tolias@gmail.com>
2022-05-26 19:58:53 +0100

    consolidate implementation, attributes

--- source/rectangle.d
+++ source/rectangle.d
@@ -10,70 +10,16 @@ module geometry.rectangle;
 import geometry.vector;
 import std.traits;
 
-///T can be any numeric type.
+///
 struct Rectangle(PositionType, SizeType)
 if(isNumeric!PositionType && isNumeric!(SizeType))
 {
-	Vector2!PositionType position; ///
-	Vector2!SizeType size; /// rectangles may have sizes of zero, but may never be negative.
-
-	invariant (size.x >= 0);
-	invariant (size.y >= 0);
-
-	/// x component of position.
-	@property @safe @nogc nothrow pure PositionType x() const
-	{
-		return position.x;
-	}
-
-	/// ditto
-	@property @safe @nogc nothrow void x(in PositionType rhs)
-	{
-		position.x = rhs;
-	}
-
-	/// y component of position.
-	@property @safe @nogc nothrow pure PositionType y() const
-	{
-		return position.y;
-	}
-
-	/// ditto
-	@property @safe @nogc nothrow void y(in PositionType rhs)
-	{
-		position.y = rhs;
-	}
-
-	/// width component of size.
-	@property @safe @nogc nothrow pure SizeType width() const
-	{
-		return size.x;
-	}
-
-	/// ditto
-	@property @safe @nogc nothrow void width(in SizeType rhs)
-	{
-		size.x = rhs;
-	}
-
-	/// ditto
-	alias w = width;
-
-	/// height component of size.
-	@property @safe @nogc nothrow pure SizeType height() const
-	{
-		return size.y;
-	}
-
-	/// ditto
-	@property @safe @nogc nothrow void height(in SizeType rhs)
-	{
-		size.y = rhs;
-	}
-
-	/// ditto
-	alias h = height;
+	///
+	mixin actAsRectangle!(PositionType, SizeType);
 }
+
+/// ditto
+alias Rectangle(T) = Rectangle!(T, T);
 
 /**
 * Returns: whether a point is within the bounds of the rectangle.
@@ -81,80 +27,20 @@ if(isNumeric!PositionType && isNumeric!(SizeType))
 *	r = a rectangle
 *	point = a 2d vector
 */
-bool pointInRectangle(R : Rectangle!(T, U), T, U, V)(in R r, in Vector2!(V) point) pure @safe @nogc nothrow
+bool pointInRectangle(R : Rectangle!(T, U), T, U, V)(in R r, in Vector2!(V) point) @nogc nothrow pure @safe
 {
 	return ((point.x >= r.x) && (point.x <= (r.x + r.w))) && ((point.y >= r.y) && (point.y <= (r.y + r.h)));
 }
 
 /// ditto
-bool pointInRectangle(S, V)(in Vector2!S size, in Vector2!(V) point) pure @safe @nogc nothrow
+bool pointInRectangle(S, V)(in Vector2!S size, in Vector2!(V) point) @nogc nothrow pure @safe
 {
-	immutable zero = Vector2!ubyte (0, 0);
+	immutable zero = Vector2!ubyte (0, 0); //todo: zero, one
 	immutable r = Rectangle!(ubyte, S) (zero, size);
 	return pointInRectangle(r, point);
 }
 
-
-alias Rectangle(T) = Rectangle!(T, T);
-
-///allows a struct to act as, and be cast to, a rectangle.
-mixin template actAsRectangle(PositionType, SizeType)
-{
-	Vector2!(PositionType) position;
-	Vector2!(SizeType) size;
-
-	@property x() const @safe @nogc pure nothrow
-	{
-		return position.x;
-	}
-
-	@property y() const @safe @nogc pure nothrow
-	{
-		return position.y;
-	}
-
-	@property void x(in PositionType rhs) @safe @nogc nothrow
-	{
-		position.x = rhs;
-	}
-
-	@property void y(in PositionType rhs) @safe @nogc nothrow
-	{
-		position.y = rhs;
-	}
-
-	@property w() const @safe @nogc pure nothrow
-	{
-		return size.x;
-	}
-
-	@property h() const @safe @nogc pure nothrow
-	{
-		return size.y;
-	}
-
-	@property void w(in SizeType rhs) @safe @nogc nothrow
-	{
-		size.x = rhs;
-	}
-
-	@property void h(in SizeType rhs) @safe @nogc nothrow
-	{
-		size.y = rhs;
-	}
-
-	Rectangle opCast(Rectangle)() pure @safe @nogc nothrow const
-	{
-		return Rectangle(position, size);
-	}
-}
-
-/// ditto
-mixin template actAsRectangle(T)
-{
-	mixin actAsRectangle!(T, T);
-}
-
+///
 @safe @nogc nothrow unittest
 {
 	immutable r = Rectangle!int(Vector2!(int)(0), Vector2!(int)(32));
@@ -170,4 +56,83 @@ mixin template actAsRectangle(T)
 	assert(r2.pointInRectangle(Vector2!(int)(ubyte.max)));
 
 	assert(pointInRectangle(Vector2!(int)(5), Vector2!(int)(2)));
+}
+
+///allows a struct to act as, and be cast to, a rectangle.
+mixin template actAsRectangle(PositionType, SizeType)
+if(isNumeric!PositionType && isNumeric!(SizeType))
+{
+	Vector2!PositionType position; ///
+	Vector2!SizeType size; /// rectangles may have sizes of zero, but may never be negative.
+
+	invariant (size.x >= 0);
+	invariant (size.y >= 0);
+
+	/// x component of position.
+	@property PositionType x() const @nogc nothrow pure @safe
+	{
+		return position.x;
+	}
+
+	/// ditto
+	@property void x(in PositionType rhs) @nogc nothrow pure @safe
+	{
+		position.x = rhs;
+	}
+
+	/// y component of position.
+	@property PositionType y() const @nogc nothrow pure @safe
+	{
+		return position.y;
+	}
+
+	/// ditto
+	@property void y(in PositionType rhs) @nogc nothrow pure @safe
+	{
+		position.y = rhs;
+	}
+
+	/// width component of size.
+	@property SizeType width() const @nogc nothrow pure @safe
+	{
+		return size.x;
+	}
+
+	/// ditto
+	@property void width(in SizeType rhs) @nogc nothrow pure @safe
+	{
+		size.x = rhs;
+	}
+
+	/// ditto
+	alias w = width;
+
+	/// height component of size.
+	@property SizeType height() const @nogc nothrow pure @safe
+	{
+		return size.y;
+	}
+
+	/// ditto
+	@property void height(in SizeType rhs) @nogc nothrow pure @safe
+	{
+		size.y = rhs;
+	}
+
+	/// ditto
+	alias h = height;
+
+	///
+	Rectangle opCast(Rectangle)() const @nogc nothrow pure @safe
+	{
+		return Rectangle(position, size);
+	}
+}
+
+/// ditto
+mixin template actAsRectangle(T)
+if (isNumeric!T)
+{
+	///
+	mixin actAsRectangle!(T, T);
 }
